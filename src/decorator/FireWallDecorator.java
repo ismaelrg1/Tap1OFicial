fe6bbfe8883693a721983a414b2759b298c40416
Ismael Ruiz <ismael.ruiz@estudiants.urv.cat>
2022-12-04 19:25:22 +0100

    Rama Ismael Creada

--- src/decorator/FireWallDecorator.java
+++ src/decorator/FireWallDecorator.java
@@ -1,4 +1,22 @@
 package decorator;
 
-public class FireWallDecorator {
+import actor.*;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class FireWallDecorator implements ActorInterface {
+    @Override
+    public void send(MessageInterface msg) {
+
+    }
+
+    @Override
+    public LinkedBlockingQueue<MessageInterface> getMsgQueue() {
+        return null;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
--- src/reflectionDinamicProxy/DynamicProxy.java
+++ src/reflectionDinamicProxy/DynamicProxy.java
@@ -1,4 +1,22 @@
 package reflectionDinamicProxy;
 
-public class DynamicProxy {
+import actor.*;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class DynamicProxy implements ActorInterface {
+    @Override
+    public void send(MessageInterface msg) {
+
+    }
+
+    @Override
+    public LinkedBlockingQueue<MessageInterface> getMsgQueue() {
+        return null;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
